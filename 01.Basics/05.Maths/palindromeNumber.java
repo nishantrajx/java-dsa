731539c0cc97928a114942f8cee64a452d1a0440
Nishant Raj <nishantrajx@gmail.com>
2026-08-03 02:30:25 +0530

    Solve LeetCode 9 - Palindrome Number

---
+++ 01.Basics/05.Maths/palindromeNumber.java
@@ -0,0 +1,26 @@
+public class palindromeNumber {
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        int x = 121;
+
+        System.out.println("Number: " + x);
+        System.out.println("Is Palindrome: " + isPalindrome(x));
+    }
+}
